47574508b62505cb56ca26aac6d2992bfb157f89
Jérémy Simonklein Angély <jeremy.simonklein@gmail.com>
2019-07-01 12:28:39 +0200

    Updates

---
+++ precache-manifest.47c9a27d8bea78d1d98c3b2ccf393287.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b3971181e0c1d0d59aaa65128712fe7",
+    "url": "/me/index.html"
+  },
+  {
+    "revision": "06e0a8ff8b208b00f7e9",
+    "url": "/me/static/css/main.5c0f462a.chunk.css"
+  },
+  {
+    "revision": "a73bc630f4fdd45128fb",
+    "url": "/me/static/js/2.e0892437.chunk.js"
+  },
+  {
+    "revision": "06e0a8ff8b208b00f7e9",
+    "url": "/me/static/js/main.10ff1aa5.chunk.js"
+  },
+  {
+    "revision": "66be97a19e235fa95a6c",
+    "url": "/me/static/js/runtime~main.7f73a39c.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/me/precache-manifest.8303218cefb6c70744375501eb23488d.js"
+  "/me/precache-manifest.47c9a27d8bea78d1d98c3b2ccf393287.js"
 );
 
 self.addEventListener('message', (event) => {
